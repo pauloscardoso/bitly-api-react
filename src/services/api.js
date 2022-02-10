943cdcd2644f38caba91d41b9cf3ab751dbd3e08
Paulo <59941082+pauloscardoso@users.noreply.github.com>
2022-02-10 17:14:20 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const key = "ad531991bfee4d6a64878138153c1bf2fe29f30e"
+export const key = 
 
 const api = axios.create({
   baseURL: 'https://api-ssl.bitly.com/v4',
